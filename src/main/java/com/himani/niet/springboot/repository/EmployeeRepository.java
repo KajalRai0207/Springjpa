64547943de7dea615c8e39b766330c531da982a5
KajalRai0207 <raikajal0207@gmail.com>
2023-02-03 09:58:28 +0530

    okk

--- src/main/java/com/himani/niet/springboot/repository/EmployeeRepository.java
+++ src/main/java/com/himani/niet/springboot/repository/EmployeeRepository.java
@@ -7,11 +7,9 @@ import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
 import com.himani.niet.springboot.model.Employee;
 
-@Repository 
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
     ArrayList<Employee> findAll();
     
